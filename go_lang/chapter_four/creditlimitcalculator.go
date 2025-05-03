dd0874e73d8a13c77905e2194cd6cef74f19f380
tecmalik <abdulmalikojo2@gmail.com>
2025-05-04 00:05:45 +0100

    commiting chapter 4

--- go_lang/chapter_four/creditlimitcalculator.go
+++ go_lang/chapter_four/creditlimitcalculator.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	fmt.Println("Credit Limit Calculator")
+	fmt.Println("======================")
+	fmt.Print("Enter account number: \n")
+	var accountNumber string
+	_, err := fmt.Scan(&accountNumber)
+	if err != nil {
+		fmt.Println("Error reading input")
+	}
+	var beginningBalance int32
+	fmt.Print("Enter beginning balance :\n")
+	_, err = fmt.Scan(&beginningBalance)
+	if err != nil {
+		fmt.Println("Error reading input")
+	}
+	fmt.Print("Enter ending balance :\n")
 
 }
