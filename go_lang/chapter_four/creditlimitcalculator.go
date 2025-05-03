f99f722f35f20075d85c16552e6e2c22084736b2
tecmalik <abdulmalikojo2@gmail.com>
2025-05-04 00:46:01 +0100

    commiting chapter 4.18

--- go_lang/chapter_four/creditlimitcalculator.go
+++ go_lang/chapter_four/creditlimitcalculator.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	fmt.Println("Credit Limit Calculator")
@@ -14,11 +12,40 @@ func main() {
 		fmt.Println("Error reading input")
 	}
 	var beginningBalance int32
-	fmt.Print("Enter beginning balance :\n")
+	fmt.Print("Enter beginning balance of the month :\n")
 	_, err = fmt.Scan(&beginningBalance)
 	if err != nil {
 		fmt.Println("Error reading input")
 	}
 	fmt.Print("Enter ending balance :\n")
+	var totalChargedItems int32
+	fmt.Print("Enter total charged items of the month :\n")
+	_, err = fmt.Scan(&totalChargedItems)
+	if err != nil {
+		fmt.Println("Error reading input")
+	}
+	var totalCredits int32
+	fmt.Print("Enter total credits of the month :\n")
+	_, err = fmt.Scan(&totalCredits)
+	if err != nil {
+		fmt.Println("Error reading input")
+	}
+	var allowedCreditLimits int32
+	fmt.Print("Enter allowed credits of the month :\n")
+	_, err = fmt.Scan(&allowedCreditLimits)
+	if err != nil {
+		fmt.Println("Error reading input")
+	}
 
+	newBalance := beginningBalance + totalChargedItems - totalCredits
+	if newBalance < allowedCreditLimits {
+		fmt.Println("You do not have enough credits on this month ")
+		fmt.Println("new balance = ", newBalance)
+		fmt.Println("allowed credit limit = ", allowedCreditLimits)
+	} else {
+		fmt.Println("You do not have enough credits on this month ")
+		fmt.Println("new balance = ", newBalance)
+		fmt.Println("allowed credit limit = ", allowedCreditLimits)
+
+	}
 }
